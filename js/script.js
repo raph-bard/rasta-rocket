444e6bf966348229e401bb8a1d4f18ead03a7619
Seydou6 <cecedric1324@gmail.com>
2023-09-26 15:04:18 +0100

    debut d'anim

---
+++ js/script.js
@@ -0,0 +1,10 @@
+
+
+// animation burger menu //
+
+const burger = document.querySelector('.burger');
+const menu = document.querySelector('.menu');
+
+burger.addEventListener('click', function() {
+  menu.classList.toggle('active'); 
+});
