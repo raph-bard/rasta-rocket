859e1695154bd00f6949c888eae9411594ae8b4c
raph-bard <raphael.bard.pro@googlemail.com>
2023-09-27 14:24:45 +0200

    commit du 27-09

--- js/script.js
+++ js/script.js
@@ -6,5 +6,6 @@ const burger = document.querySelector('.burger');
 const menu = document.querySelector('.menu');
 
 burger.addEventListener('click', function() {
-  menu.classList.toggle('active'); 
+  menu.classList.toggle('active');
+  burger.classList.toggle('active');
 });
