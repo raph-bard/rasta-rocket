1d976f51390979be2c2556bd9bc84759dc89b43e
raph-bard <raphael.bard.pro@googlemail.com>
2023-09-28 10:20:34 +0200

    scroll burger menu enlevé + bugfix

--- js/script.js
+++ js/script.js
@@ -13,10 +13,19 @@ import {bars, createCardMobile} from './data.mjs'
 
 const burger = document.querySelector('.burger');
 const menu = document.querySelector('.menu');
-const body = document.querySelector('body');
+const body = document.body;
+const largeurBarreDeDefilement = window.innerWidth - document.documentElement.clientWidth; //calcul de la largeur de la scrollbar
 
+//fonction permettant d'afficher/cacher le menu quand on appuie sur le boutton burger
 burger.addEventListener('click', function() {
   menu.classList.toggle('active');
   burger.classList.toggle('active');
   body.classList.toggle('noscroll');
+
+  // condition permettant d'enlever le décallage du burger menu quand la scrollbar disparait
+  if (body.classList.contains('noscroll')) {
+    burger.style.right = 27 + largeurBarreDeDefilement + 'px'; 
+    } else {
+      burger.style.right = '27px';
+    }
 });
