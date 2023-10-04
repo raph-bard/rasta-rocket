b5277533e041b9f3ebf02b8ac2f2e6bdb4b8778b
Mahdi Mcheik <mahdi.mcheik@hotmail.fr>
2023-10-04 22:05:10 +0200

    adding style to icons , paddins and color

--- js/bar-detail.js
+++ js/bar-detail.js
@@ -11,7 +11,7 @@ let createBarDetailPage = (bar) => {
     <div class="bandeau-container">
       <div
         class="bandeau"
-        style="background-image: url(../${bar.img})"
+        style="background-image: url(${bar.img})"
       ></div>
     </div>
 
