1af622998066d34c43261e2ddfb448889cd8704f
mahdi mcheik <mahdi.mcheik@hotmail.fr>
2023-10-05 12:13:58 +0200

    faking loading at start time only, smaller delay for pages trasitions

--- js/index.js
+++ js/index.js
@@ -1,25 +1,41 @@
 //******************** Introduction Shaker Animé ********************//
 
-if (window.location.pathname === "/index.html") {
-  // Ajoute la classe "no-scroll" à la balise HTML dès le début, la condition sert à vérifier si on est bien sur la page index.
-  document.documentElement.classList.add("noscroll");
+// s'active aussi après 4.5s
+/******* handling animations *******/
+if (!!sessionStorage.getItem("visited")) {
+  document.querySelector(".shaker-container").remove();
+  let bandeauP = document
+    .querySelector(".bandeau p")
+    .classList.remove("opacity-delayed-index");
+  let bandeauH = document
+    .querySelector(".bandeau h1")
+    .classList.remove("opacity-delayed-index");
+  let containerIndex = document.querySelector(".container");
+  containerIndex.classList.remove("opacity-delayed2-index");
+  containerIndex.classList.add("opacity-delayed");
+  document.documentElement.classList.remove("noscroll");
+} else {
+  sessionStorage.setItem("visited", true);
+  if (window.location.pathname === "/index.html") {
+    // Ajoute la classe "no-scroll" à la balise HTML dès le début, la condition sert à vérifier si on est bien sur la page index.
+    document.documentElement.classList.add("noscroll");
 
-  // Retire la classe "noscroll" de la balise HTML après 4,5 secondes
-  setTimeout(function () {
-    document.documentElement.classList.remove("noscroll");
-  }, 4500); // s'active après 4.5s
-}
-
-var deleteShaker = document.querySelector(".shaker-container");
-
-// setTimeout sert à définir un délai
-setTimeout(function () {
-  if (deleteShaker) {
-    // Supprime l'html de l'animation pour ne pas créer de bugs de z-index une fois qu'il disparait
-    deleteShaker.parentNode.removeChild(deleteShaker);
+    // Retire la classe "noscroll" de la balise HTML après 4,5 secondes
+    setTimeout(function () {
+      document.documentElement.classList.remove("noscroll");
+    }, 4500); // s'active après 4.5s
   }
-}, 4500); // s'active aussi après 4.5s
 
+  var deleteShaker = document.querySelector(".shaker-container");
+
+  // setTimeout sert à définir un délai
+  setTimeout(function () {
+    if (deleteShaker) {
+      // Supprime l'html de l'animation pour ne pas créer de bugs de z-index une fois qu'il disparait
+      deleteShaker.parentNode.removeChild(deleteShaker);
+    }
+  }, 4500);
+}
 /*      Creation Tableau d'objets bars         */
 
 import bars from "./data.js";
